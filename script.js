5fc1081603e38f22e0251a62d787fa0302034c9a
brielznx <gabriel.celestino27@escola.pr.gov.br>
2026-08-06 10:42:24 -0300

    ok

---
+++ script.js
@@ -0,0 +1,15 @@
+//Botoes 
+
+const btnContraste = document.getElementbyId("bnt-contraste");
+const btAumentar = document.getElementbyId("btn-aumentar-texto")
+const btDiminuir = document.getElementbyId("btn-diminuir-texto")
+
+//Mudar tamanho da fonte
+
+let tamanhoAtualFonte = 100;
+
+//Alto constraste
+
+btnContraste.addEventListener("Click", ()=>{
+
+})
