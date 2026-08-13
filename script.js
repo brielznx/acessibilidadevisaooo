88e51eed879c8be45f3938ed296caeadb3c59abe
brielznx <gabriel.celestino27@escola.pr.gov.br>
2026-08-13 10:42:43 -0300

    ok

--- script.js
+++ script.js
@@ -11,5 +11,42 @@ let tamanhoAtualFonte = 100;
 //Alto constraste
 
 btnContraste.addEventListener("Click", ()=>{
+    document.body.classList.toggle("alto-contraste");
 
-})
+    //leitor de tela
+
+    const ativo = document.body.classList.contains("alto-contraste");
+    btnContraste.setAttribute("aria-pressed",ativo);
+
+
+});
+
+//Aumentar o erro
+
+
+btAumentar.addEventListener("click", () =>{
+    if (tamanhoAtualFonte < 150) {
+        tamanhoAtualFonte +=10;
+        document.documentElement.style.fontSize = `${tamanhoAtualFonte}%`
+
+
+
+    }
+
+});
+
+//diminuir o texto
+
+btDiminuir.add("click",() =>{
+    if (tamanhoAtualFonte > 90){
+        tamanhoAtualFonte -=10;
+        document.documentElement.style.fontSize = `${tamanhoAtualFonte}%`;
+
+    }
+
+
+
+});
+});
+
+
